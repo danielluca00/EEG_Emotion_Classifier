53df7abbc8d827287afe8f620ce0797d700e98b3
danielluca00 <dani28settembre@gmail.com>
2025-11-05 09:32:35 +0100

    simplified DNN model

--- models/dnn_model.py
+++ models/dnn_model.py
@@ -3,29 +3,22 @@ from tensorflow.keras import Model, Input
 from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 
 def create_dnn(input_dim, num_classes=3):
-    i_dnn = Input(shape=(input_dim,))
-
-    x = Dense(2548, activation='relu')(i_dnn)
-    x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
-
-    x = Dense(3822, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Dropout(0.27)(x)
-
-    x = Dense(5096, activation='relu')(x)
+    i = Input(shape=(input_dim,))
+    
+    x = Dense(512, activation='relu')(i)
     x = BatchNormalization()(x)
     x = Dropout(0.3)(x)
 
-    x = Dense(3822, activation='relu')(x)
+    x = Dense(256, activation='relu')(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.27)(x)
+    x = Dropout(0.3)(x)
 
-    x = Dense(2548, activation='relu')(x)
+    x = Dense(128, activation='relu')(x)
     x = BatchNormalization()(x)
     x = Dropout(0.25)(x)
 
     y = Dense(num_classes, activation='softmax')(x)
-
-    model = Model(inputs=i_dnn, outputs=y)
+    
+    model = Model(inputs=i, outputs=y)
     return model
+
